5c8d1620b1347241324e775c66122bbf437e5f50
Joseph Nishimwe <j.nishimwe@alustudent>
2024-11-07 19:45:52 -0800

    student.py

---
+++ student.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self, name, gender, grades):
+        self.name = name
+        self.gender = gender
+        self.grades = grades
+
+    def calculate_average(self):
+        return sum(self.grades) / len(self.grades)
